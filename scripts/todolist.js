936cb81271298c19415da0f78cdec444b1a7e75d
Igor-CA <122459409+Igor-CA@users.noreply.github.com>
2023-01-29 00:40:45 -0300

    add screen added

--- scripts/todolist.js
+++ scripts/todolist.js
@@ -1,5 +1,5 @@
 let add_btn = document.getElementById("add")
-add_btn.addEventListener("click", add)
+add_btn.addEventListener("click", add_screen)
 
 function check_boxes(){
     let check_boxes = document.querySelectorAll("main > ul > li > input[type=checkbox]")
@@ -16,15 +16,45 @@ function check_boxes(){
     }
 }
 
+function add_screen(){
+    screen = document.getElementById('add_task_screen')
+    document.body.classList.add('disabled')
+    screen.classList.remove('unvisible')
+}
+
 function add(){
     list = document.getElementById('todo-list')
     list_size = document.querySelectorAll("main > ul > li").length
     
+    screen = document.getElementById('add_task_screen')
+    
+    task_name = document.getElementById('add_task')
     list_item = document.createElement('li')
     list_item.classList.add('undone') 
     
-    list_item.innerHTML = `<input type="checkbox" name="task${list_size}" id="task${list_size}">
-    <label for="task${list_size}">Task${list_size}</label>`
-    list.appendChild(list_item)
-    window.scrollTo(0, document.body.scrollHeight);
+
+    if(task_name.value.length > 0){
+        list_item.innerHTML = `<input type="checkbox" name="task${list_size}" id="task${list_size}">
+        <label for="task${list_size}">${task_name.value}</label>`
+        task_name.value = null
+        screen.classList.add('unvisible')
+        document.body.classList.remove('disabled')
+        list.appendChild(list_item)
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+    else{
+        task_name.placeholder = "Add tesk here"
+        task_name.focus()
+    }
+}
+function check_save(){
+    console.log('aqui')
+    save_button = document.getElementById('save_task_button')
+    task_name = document.getElementById('add_task')
+    if(task_name.value.length > 0){
+        save_button.style.color = 'var(--cor005)'
+    }
+    else{
+        save_button.style.color = '#4a4a4a'        
+    }
 }
