e6ce62e58efe95b0f183ec99789a90b6f4589407
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2023-02-01 12:40:30 -0300

    Added catergory selection/creation

--- scripts/todolist.js
+++ scripts/todolist.js
@@ -3,17 +3,14 @@ let screen = document.getElementById('add_task_screen')
 
 add_btn.addEventListener("click", show_add_screen)
 
-let categorys = document.querySelectorAll('#categorys>li')
-for(let index=0; index<categorys.length; index++){
-    categorys[index].addEventListener('click', () => { select_category(index) });
-}
 
-function select_category(value){
-    for(let index=0; index<categorys.length; index++){
-        categorys[index].classList.remove('selected')
+function select_category(selected_category){
+    let categorys = document.querySelectorAll('#categorys>li')
+    for(let i=0; i<categorys.length; i++){
+        categorys[i].classList.remove('selected')
     }
-    categorys[value].classList = 'selected'
-    document.querySelector('main>h1').innerHTML = categorys[value].textContent
+    selected_category.classList = 'selected'
+    document.querySelector('main>h1').innerHTML =selected_category.textContent
 }
 
 function check_tasks(){
@@ -49,9 +46,11 @@ function order_tasks(){
 function show_add_screen(){
     document.body.classList.add('disabled')
     screen.classList.remove('unvisible')
-    var task_name_input = document.getElementById('add_task')
+    let task_name_input = document.getElementById('new_task_name')
+    let category_input = document.getElementById('category_input')
     task_name_input.value = ''
     task_name_input.focus()
+    category_input.value = ''
     reset_date()
 }
 
@@ -70,7 +69,7 @@ function add_new_task(){
     let tasks_list = document.getElementById('todo-list')
     let task_list_size = document.querySelectorAll("main > ul > li").length
     
-    let task_name_input = document.getElementById('add_task')
+    let task_name_input = document.getElementById('new_task_name')
     let task_name = task_name_input.value
     
     let list_item = document.createElement('li')
@@ -87,11 +86,41 @@ function add_new_task(){
         window.scrollTo(0, document.body.scrollHeight);
         task_name_input.value = ''
         order_tasks()
+        set_category()
     }
     else{
         task_name_input.placeholder = "Add tesk here"
         task_name_input.focus()
     }
+}
+
+function set_category(){
+    let category_input = document.getElementById('category_input')
+    let categorys_data_list = document.getElementById('categorys_list')
+    let item = document.createElement('option')
+    item.value = category_input.value
+    if(!check_in_category_list(item)){
+        categorys_data_list.appendChild(item)
+        add_to_category_selector(item.value)
+    }
+}
+
+function add_to_category_selector(category_name){
+    let categorys_list = document.getElementById('categorys')
+    let item = document.createElement('li')
+    item.innerText = category_name
+    item.setAttribute('onclick',' select_category(this)')
+    categorys_list.appendChild(item)
+}
+
+function check_in_category_list(item){
+    let category = document.querySelectorAll("#categorys_list > option")
+    for(let i=0; i<category.length; i++){
+        if(item.value == category[i].value){
+            return true
+        }
+    }
+    return false
 }
 
 function create_task_html(task_index, task_name){
